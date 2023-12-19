c91f608f5c2d2291cba5a8c87ce7194abd4b36bd
Nataliia-Homyk324 <homyk324@gmail.com>
2023-12-19 11:56:52 +0200

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,21 @@
+const form = document.querySelector('.login-form');
 
+  form.addEventListener('submit', function (event) {
+    event.preventDefault(); // відміна перезавантаження сторінки
+
+    let email = form.elements.email.value.trim();
+    let password = form.elements.password.value.trim();
+
+    if (email === '' || password === '') {
+      alert('All form fields must be filled in');
+    } else {
+      let formData = {
+        email: email,
+        password: password
+      };
+
+      console.log(formData);
+
+      form.reset(); // очищення значень полів форми
+    }
+  });
